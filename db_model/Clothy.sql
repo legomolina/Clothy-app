25ff6080b71d8b697dc774812328e2ff66ee8c6e
legomolina <legomolina@gmail.com>
2017-04-29 15:21:14 +0200

    Reformatted users table to employees

--- db_model/Clothy.sql
+++ db_model/Clothy.sql
@@ -4,15 +4,15 @@ COLLATE utf8_general_ci;
 
 USE Clothy;
 
-CREATE TABLE IF NOT EXISTS users (
-	user_id bigint(11) NOT NULL AUTO_INCREMENT,
-    user_name varchar(255) NOT NULL,
-    user_surname varchar(255) DEFAULT '',
-    user_address text,
-    user_email varchar(255) NOT NULL,
-    user_phone varchar(20) NOT NULL,
-    user_login_name varchar(255) NOT NULL,
-    user_login_password varchar(255) NOT NULL,
-    user_login_type varchar(20) NOT NULL DEFAULT 'TYPE_USER',
-	CONSTRAINT pk_users PRIMARY KEY (user_id)
+CREATE TABLE IF NOT EXISTS employees (
+	employee_id bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    employee_name varchar(255) NOT NULL,
+    employee_surname varchar(255) DEFAULT '',
+    employee_address text,
+    employee_email varchar(255) NOT NULL,
+    employee_phone varchar(20) NOT NULL,
+    employee_login_name varchar(255) NOT NULL,
+    employee_login_password varchar(255) NOT NULL,
+    employee_login_type varchar(20) NOT NULL DEFAULT 'TYPE_USER',
+	CONSTRAINT pk_employee PRIMARY KEY (employee_id)
 );
