6a8dc96056004d68f424ba3e662ada3523493d3c
legomolina <legomolina@gmail.com>
2017-04-29 02:20:32 +0200

    Updated Database Model

--- db_model/Clothy.sql
+++ db_model/Clothy.sql
@@ -1,5 +1,18 @@
-CREATE DATABASE Clothy
+CREATE DATABASE IF NOT EXISTS Clothy
 CHARACTER SET utf8
 COLLATE utf8_general_ci;
 
 USE Clothy;
+
+CREATE TABLE IF NOT EXISTS users (
+	user_id bigint(11) NOT NULL AUTO_INCREMENT,
+    user_name varchar(255) NOT NULL,
+    user_surname varchar(255) DEFAULT '',
+    user_address text,
+    user_email varchar(255) NOT NULL,
+    user_phone varchar(20) NOT NULL,
+    user_login_name varchar(255) NOT NULL,
+    user_login_password varchar(255) NOT NULL,
+    user_login_type varchar(20) NOT NULL DEFAULT 'TYPE_USER',
+	CONSTRAINT pk_users PRIMARY KEY (user_id)
+);
---
+++ src/config/Constants.java
@@ -0,0 +1,9 @@
+package config;
+
+public final class Constants {
+    public static final class Database {
+        public final static String URL = "jdbc:mysql://localhost/clothy";
+        public final static String USER = "cristian";
+        public final static String PASSWORD = "1q2w3e12";
+    }
+}
