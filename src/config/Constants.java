b6c0d84fd1241baf1a5e2d3fc9f74be708be2140
legomolina <legomolina@gmail.com>
2017-04-29 20:11:38 +0200

    remove config folder from git

--- src/config/Constants.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-public final class Constants {
-    public static final class Database {
-        public final static String URL = "jdbc:mysql://localhost/clothy";
-        public final static String USER = "cristian";
-        public final static String PASSWORD = "1q2w3e12";
-    }
-}
