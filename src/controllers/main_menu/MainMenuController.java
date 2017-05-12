c0fab60eb6e969132015be59b468f100336443bc
legomolina <legomolina@gmail.com>
2017-05-12 21:41:02 +0200

    Removed Open Sans Font, fixed Masonry Pane for Main menu

--- src/controllers/main_menu/MainMenuController.java
+++ src/controllers/main_menu/MainMenuController.java
@@ -2,17 +2,13 @@ package controllers.main_menu;
 
 
 import com.jfoenix.controls.JFXMasonryPane;
-import com.jfoenix.controls.JFXRippler;
 import com.sun.javafx.stage.StageHelper;
 import controllers.BaseController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Cursor;
 import javafx.scene.control.Label;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import models.Employee;
 import views.BaseStage;
@@ -26,11 +22,6 @@ public class MainMenuController extends BaseController {
 
     @FXML private Label welcomeLabel;
     @FXML private Pane titleContainer;
-    @FXML private Pane employeesButton;
-    @FXML private Pane articlesButton;
-    @FXML private Pane clientsButton;
-    @FXML private Pane salesButton;
-    @FXML private Pane ordersButton;
     @FXML private JFXMasonryPane menuButtons;
 
     @FXML
@@ -45,7 +36,7 @@ public class MainMenuController extends BaseController {
         }
 
         if (!opened)
-            new Employees((Stage) employeesButton.getScene().getWindow(), loggedEmployee);
+            new Employees((Stage) menuButtons.getScene().getWindow(), loggedEmployee);
     }
 
     @FXML
@@ -60,7 +51,7 @@ public class MainMenuController extends BaseController {
         }
 
         if (!opened)
-            new Clients((Stage) clientsButton.getScene().getWindow(), loggedEmployee);
+            new Clients((Stage) menuButtons.getScene().getWindow(), loggedEmployee);
     }
 
     @Override
@@ -106,36 +97,12 @@ public class MainMenuController extends BaseController {
 
     private void setListeners() {
         titleContainer.setEffect(new BoxBlur(5, 5, 3));
-
-        JFXRippler employeesRipple = new JFXRippler(employeesButton);
-        employeesRipple.setCursor(Cursor.HAND);
-        employeesRipple.setRipplerFill(Paint.valueOf("#ffd351"));
-        menuButtons.getChildren().add(employeesRipple);
-
-        JFXRippler articlesRipple = new JFXRippler(articlesButton);
-        articlesRipple.setCursor(Cursor.HAND);
-        articlesRipple.setRipplerFill(Paint.valueOf("#e24848"));
-        menuButtons.getChildren().add(articlesRipple);
-
-        JFXRippler clientsRipple = new JFXRippler(clientsButton);
-        clientsRipple.setCursor(Cursor.HAND);
-        clientsRipple.setRipplerFill(Paint.valueOf("#00c8ff"));
-        menuButtons.getChildren().add(clientsRipple);
-
-        JFXRippler salesRipple = new JFXRippler(salesButton);
-        salesRipple.setCursor(Cursor.HAND);
-        salesRipple.setRipplerFill(Paint.valueOf("#7856ff"));
-        menuButtons.getChildren().add(salesRipple);
-
-        JFXRippler ordersRipple = new JFXRippler(ordersButton);
-        ordersRipple.setCursor(Cursor.HAND);
-        ordersRipple.setRipplerFill(Paint.valueOf("#76e285"));
-        menuButtons.getChildren().add(ordersRipple);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         welcomeLabel.setText("Bienvenido " + loggedEmployee.getName());
+        menuButtons.setLayoutMode(JFXMasonryPane.LayoutMode.MASONRY);
         setListeners();
     }
 }
